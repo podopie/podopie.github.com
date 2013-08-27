a698a498217cb9124eabf80ad0873ac9ca4864d6
Ed Podojil <ed@animoto.com>
2013-08-27 11:57:27 -0400

    because half!

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,29 +1,22 @@
 $('.h-icon').click(function(){
-  var activeState = $(this).html().split(' ').join('').toLowerCase();
-  $('.h-icon').removeClass('active');
-  $(this).addClass('active');
-  $('.position').removeClass(function (index, css) {
+  var a = 'active',
+    c = 'class',
+    p = 'position',
+    s = ['.about', '.blog', '.portfolio', '.findme'],
+    gr = function(x,i){
+    return $(x).attr(c).split(' ')[i];
+  }
+  $('.'+a).removeClass(a);
+  $(this).addClass(a);
+  cC = gr($(this), 1).split('-')[2];
+  cP = gr('.' + cC, 2)[8] - 3;
+  var nP = s.map(function(x){
+    return gr($(x), 2)[8] - cP;
+  });
+  $('.' + p).removeClass(function(x, css) {
     return (css.match (/\bposition[0-9]$/g) || []).join(' ');
   });
-  if (activeState === 'about') {
-    $('.about').addClass('position3');
-    $('.blogs').addClass('position4');
-    $('.portfolio').addClass('position5');
-    $('.findme').addClass('position6');
-  } else if (activeState === 'blog') {
-    $('.about').addClass('position2');
-    $('.blogs').addClass('position3');
-    $('.portfolio').addClass('position4');
-    $('.findme').addClass('position5');
-  } else if (activeState === 'portfolio') {
-    $('.about').addClass('position1');
-    $('.blogs').addClass('position2');
-    $('.portfolio').addClass('position3');
-    $('.findme').addClass('position4');
-  } else if (activeState === 'findme') {
-    $('.about').addClass('position0');
-    $('.blogs').addClass('position1');
-    $('.portfolio').addClass('position2');
-    $('.findme').addClass('position3');
-  }
-})
+  s.map(function(x, i){
+    $(x).addClass(p + nP[i])
+  });
+});
